57643d8882bba1610c1219e6d9aff47831d15628
shuklashivmohan5 <shuklashivmohan19@gmail.com>
2024-06-14 12:07:56 +0530

    done

---
+++ L1/assignment.js
@@ -0,0 +1,25 @@
+
+// Number
+ a = 1;
+console.log(a);
+console.log(typeof a);
+
+// String
+const b = "abcd";
+console.log(b);
+console.log(typeof b);
+
+// Boolen
+const c = true;
+console.log(c);
+console.log(typeof c);
+
+// null
+const d = null;
+console.log(d);
+console.log(typeof d);
+
+// Undefined 
+let e ;
+console.log(e);
+console.log(typeof e);
---
+++ L2/assignment.js
@@ -0,0 +1,36 @@
+
+// var-function scope //
+// example //
+
+// function name(){
+//     var student1 = "shiv"
+//     // console.log(student1);
+// }
+
+// var abc = student1
+
+// console.log(abc)
+
+// function name(){
+//     let student1 = "Shivmohan"
+//     if(true){
+//        let abc = student1;
+//     }
+//     console.log(abc);
+// }
+
+// name()
+
+// function name(){
+//     const student1 = "Shivmohan"
+//     console.log(student1);
+// }
+
+// name()
+
+// var challenge = "30 days of javascript"
+// console.log(challenge);
+
+
+
+
