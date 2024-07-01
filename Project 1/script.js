86893e2f67f97712ecd79969d2afbe36c06b99e9
shuklashivmohan5 <shuklashivmohan19@gmail.com>
2024-07-01 11:37:53 +0530

    done

---
+++ Project 1/script.js
@@ -0,0 +1,33 @@
+// Que-1 Javascript program to swape 2 variables.
+// Que-2 Javascript program find to square.
+// Que-3 Find the third angle of a right triangle if to others angle are given.
+
+
+// Que-1 Javascript program to swape 2 variables.
+
+let a = 5;
+let b = 10;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`a: ${a}, b: ${b}`); // a: 10, b: 5
+
+
+// Que-2 Javascript program find to square.
+
+let x = 24;
+   
+console.log(x*x); // x: 576
+
+
+
+// Que-3 Find the third angle of a right triangle if to others angle are given.
+
+let total,c,bside=30,aside=90;
+c=aside+bside;
+
+total = 180-c;
+
+console.log(total); total: 60
